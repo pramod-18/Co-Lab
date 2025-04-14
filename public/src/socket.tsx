4b2fae9c71fd0c071d9f3c9d6f9bc57feb535893
pramod-18 <pramod_krp@cs.iitr.ac.in>
2025-04-14 11:08:41 +0530

    commit1

--- public/src/socket.tsx
+++ public/src/socket.tsx
@@ -1,7 +1,7 @@
 // socket.js or socket.ts if using TypeScript
 import { io } from "socket.io-client";
 
-const socket = io("http://10.81.36.83:3000", {
+const socket = io("http://10.81.35.101:3000", {
   autoConnect: false, // Optional: control connection manually
 });
 
